06840e584f5c78cacd6a444ec70b9feb93c56836
linfengisep <wang.linfeng0213@gmail.com>
2018-09-19 15:24:35 +0200

    depth first search algo

--- DFSAlgo.c
+++ DFSAlgo.c
@@ -2,6 +2,10 @@
 DFS:depthward motion,uses stack to remember the vertex
 in order to explore the adjacent vertex;
 depth first search;
+
+using a matrix to represent the edge,or the connection between vertex;
+struct of vertext to store the vertex
+
 */
 #include<stdio.h>
 #include<stdbool.h>
@@ -15,11 +19,8 @@ struct Vertex{
 
 int stack[max];
 int top = -1;
-//try the matrix;
 int adjMatrix[max][max];
-//array of vertex;
 struct Vertex* listVertex[max];
-//count of vertex;
 int nbrVertex=0;
 
 void push(int item){stack[++top]=item;}
@@ -32,6 +33,7 @@ void addEdge(int start, int end){
    adjMatrix[end][start]=1;
 }
 void addVertex(char vertexLabel){
+   //build vertex from the given label;
    struct Vertex * vertex = (struct Vertex*)malloc(sizeof(struct Vertex));
    vertex->label = vertexLabel;
    //printf("label is:%c\n",vertex->label);
@@ -58,7 +60,6 @@ void displayVertex(int index){
 void depthFirstSearch(){
    int i;
    listVertex[0]->visited = true;
-   //printf("first vertext visited? %d\n",listVertex[0]->visited);
    displayVertex(0);
    //push this original vertex indew to the stack;
    push(0);
@@ -67,8 +68,8 @@ void depthFirstSearch(){
       if(unvisitedVertexIndex == -1){
          pop();
       }else{
+         //when there are vertex not visited; push it to stack ,make its vistied true;
          listVertex[unvisitedVertexIndex]->visited = true;
-         //printf("there should be label below");
          displayVertex(unvisitedVertexIndex);
          push(unvisitedVertexIndex);
       }
@@ -93,13 +94,7 @@ void loadingData(){
    addEdge(2,4);//B-D
    addEdge(3,4);//C-D
 }
-void displayMatrix(int size){
-   for(int i=0;i<size;i++){
-      for(int j=0;j<size;j++){
-         printf("adjMatrix[%d][%d]:%d\n",i,j,adjMatrix[i][j]);
-      }
-   }
-}
+
 int main(int argc, char const *argv[]) {
    //set matrix all 0 to begin;
    for(int i=0;i<max;i++){
@@ -108,7 +103,6 @@ int main(int argc, char const *argv[]) {
          }
    }
    loadingData();
-   //displayMatrix(max);
    depthFirstSearch();
    return 0;
 }
